7717dce3ccd372b750c8210572f287744b234280
Natoli Lemma Dinku <natolilemma1@gmail.com>
2025-10-22 10:40:24 -0500

    auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,42 +4,36 @@ const userRepository = require("../repositories/user.repository");
 const logger = require("../utils/logger");
 
 const authMiddleware = async (req, res, next) => {
-  // const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
-  //   return res.status(401).json({ message: "Unauthorized: No token provided" });
-  // }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
 
-  // const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
 
-  // try {
-  //   const decoded = jwt.verify(token, config.jwt.secret);
-  //   const user = await userRepository.findById(decoded.id);
+  try {
+    const decoded = jwt.verify(token, config.jwt.secret);
+    const user = await userRepository.findById(decoded.id);
 
-  //   if (!user) {
-  //     return res.status(401).json({ message: "Unauthorized: User not found" });
-  //   }
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized: User not found" });
+    }
 
-  //   // Attach user to the request object, excluding the password
-  //   req.user = user;
-  //   next();
-  // } catch (error) {
-  //   logger.error("Authentication error:", error);
-  //   if (error instanceof jwt.TokenExpiredError) {
-  //     return res
-  //       .status(401)
-  //       .json({ message: "Unauthorized: Token has expired" });
-  //   }
-  //   return res.status(401).json({ message: "Unauthorized: Invalid token" });
-  // }
+    // Attach user to the request object, excluding the password
+    req.user = user;
+    next();
+  } catch (error) {
+    logger.error("Authentication error:", error);
+    if (error instanceof jwt.TokenExpiredError) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Token has expired" });
+    }
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
 
-  req.user = {
-    firstName: "John",
-    lastName: "Doe",
-    email: "john.doe@example.com",
-    password: "password123",
-  };
-  next();
+  
 };
 
 module.exports = authMiddleware;
